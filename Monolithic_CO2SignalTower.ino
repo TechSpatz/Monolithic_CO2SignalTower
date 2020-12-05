c0101b1415f4bc4ba6d9d125a7d292c4431db86e
Chris <christian.cz@laserblog.de>
2020-12-05 22:42:02 +0100

    Update Monolithic_CO2SignalTower.ino

--- Monolithic_CO2SignalTower.ino
+++ Monolithic_CO2SignalTower.ino
@@ -18,7 +18,8 @@ int countdown;
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
 const long intervalCO2 = 5000;        //Measurement Interval
-const long intervalPre = 15000;      //Preheat Interval
+const long intervalPre = 15000;       //Preheat Interval
+const long intervalLED = 20;          //LED delay
 
 /* Recommended Settings for   
  * - Warn-Limit with Yellow Signal
@@ -40,9 +41,7 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 void setup(){            
   
   CO2Serial.begin(9600);              //Softserial
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); 
-   
+  
   u8g2.begin();                       //Display init
   u8g2.clear();
   u8g2.setFontRefHeightExtendedText();
@@ -66,25 +65,65 @@ void loop(){
 currentMillis = millis();
 MHZ19_RESULT response = mhz.retrieveData();
 
-      if (response == MHZ19_RESULT_OK && currentMillis - previousMillis >= intervalCO2){
+if (response == MHZ19_RESULT_OK && currentMillis - previousMillis >= intervalCO2){
           previousMillis = currentMillis;
           CO2 = mhz.getCO2();
           TEMP = mhz.getTemperature();
-          digitalWrite(LED_BUILTIN, LOW);
-          delay(50);
-          digitalWrite(LED_BUILTIN, HIGH); 
           if (state == false){
             CO2buffer = CO2;
             state = true;
           }
-          oled(); 
       Serial.print(CO2);
       Serial.println(" PPM");
       Serial.print(TEMP);
       Serial.println("°C");
-        }
+}
 
-  if (CO2 >= ALERT){               // Alert - Red 
+if (CO2buffer != CO2 && state == true ){           // Display Output
+  u8g2.clearBuffer();         
+  if (CO2 > CO2buffer){
+    CO2buffer++;
+    u8g2.drawTriangle(119,20, 112,28, 126,28);
+    if (CO2 - CO2buffer >= 50){
+      CO2buffer = CO2;  
+    }
+  }
+  else if (CO2 < CO2buffer){
+    CO2buffer--;
+    u8g2.drawTriangle(119,28, 112,20, 126,20);
+        if (CO2buffer - CO2 >= 50){
+      CO2buffer = CO2;  
+    }
+  }
+
+        bargraph = ((CO2buffer-400)*122/(WARN-400)); 
+     if (bargraph >= 122){
+        bargraph = 122;
+      }
+
+     u8g2.drawFrame(0, 0, 128, 16);
+     u8g2.drawBox(3, 3, bargraph, 10);
+     
+     u8g2.setCursor(0, 20);
+     u8g2.setFont(u8g2_font_logisoso42_tf);
+      if (CO2buffer <1000){
+      u8g2.print(" ");}
+
+      u8g2.print(CO2buffer);
+
+      u8g2.drawLine(110, 31, 128, 31);
+     u8g2.setCursor(110, 34);
+     u8g2.setFont(u8g2_font_profont12_tf);
+      u8g2.print("CO2");
+
+     u8g2.drawLine(110, 47, 128, 47); 
+     u8g2.setCursor(110, 48);
+      u8g2.print("ppm");
+    u8g2.drawLine(110, 63, 128, 63);
+    u8g2.sendBuffer();
+}
+   
+if (CO2 >= ALERT){               // Alert - Red 
 
   for(uint16_t i=0; i<pixels.numPixels(); i++){
     pixels.setPixelColor(i, 0,0,0);
@@ -101,7 +140,7 @@ MHZ19_RESULT response = mhz.retrieveData();
 }
 
  
-  else if (CO2 >= WARN){          // Warning - Yellow
+else if (CO2 >= WARN){          // Warning - Yellow
 
   for(uint16_t i=0; i<pixels.numPixels(); i++){
     pixels.setPixelColor(i, 0,0,0);
@@ -113,12 +152,12 @@ MHZ19_RESULT response = mhz.retrieveData();
   }
 
   pixels.show();
-  delay(40); 
+  delay(20); 
   pos = (pos + 1) % pixels.numPixels();  
-  }
+}
 
 
-  else {                          // Normal - Green
+else {                          // Normal - Green
     for(int i=0;i<pixels.numPixels();i++){
     pixels.clear();
     pixels.setPixelColor(1,0,25,0);
@@ -126,8 +165,7 @@ MHZ19_RESULT response = mhz.retrieveData();
     pixels.setPixelColor(7,0,25,0);
     pixels.setPixelColor(10,0,25,0);
     pixels.show();
-    delay(100);
-    }
+}    
   }    
 }
 
@@ -168,47 +206,4 @@ void preheat() {                            // 180 sec preheating
   }while (i<pixels.numPixels() && countdown>0);
   pixels.clear();
   u8g2.clear();
-}
-
-void oled() {                           
-
-do{
-  u8g2.clearBuffer();
-  if (CO2 > CO2buffer){
-    CO2buffer++;
-    u8g2.drawTriangle(119,20, 112,28, 126,28);  
-  }
-  else if (CO2 < CO2buffer){
-    CO2buffer--;
-    u8g2.drawTriangle(119,28, 112,20, 126,20);
-  }
-
-        bargraph = ((CO2buffer-400)*122/(WARN-400)); 
-     if (bargraph >= 122){
-        bargraph = 122;
-      }
-
-     u8g2.drawFrame(0, 0, 128, 16);
-     u8g2.drawBox(3, 3, bargraph, 10);
-     
-     u8g2.setCursor(0, 20);
-     u8g2.setFont(u8g2_font_logisoso42_tf);
-      if (CO2 <1000){
-      u8g2.print(" ");}
-
-      u8g2.print(CO2buffer);
-
-      u8g2.drawLine(110, 31, 128, 31);
-     u8g2.setCursor(110, 34);
-     u8g2.setFont(u8g2_font_profont12_tf);
-      u8g2.print("CO2");
-
-     u8g2.drawLine(110, 47, 128, 47); 
-     u8g2.setCursor(110, 48);
-      u8g2.print("ppm");
-    u8g2.drawLine(110, 63, 128, 63);
-    u8g2.sendBuffer();
-    delay(30);
-
-}while(CO2buffer != CO2); 
 }
